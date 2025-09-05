59f28ac8d444bf88ee194c4a1fe21671b2a30cc4
lyh990517 <lyh990517@naver.com>
2025-09-05 11:44:43 +0900

    version up 1.0.2

--- build-logic/task/build.gradle.kts
+++ build-logic/task/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.lyh990517"
-version = "1.0.1"
+version = "1.0.2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -35,8 +35,38 @@ gradlePlugin {
             id = "io.github.lyh990517.notion-stringboard"
             implementationClass = "task.plugin.notion.NotionStringboardPlugin"
             displayName = "Notion Stringboard Plugin"
-            description = "Generate Android string resources from Notion database"
-            tags = listOf("notion", "android", "localization", "strings")
+            description = "A Gradle plugin that automatically generates Android string resources from Notion database with multi-language support"
+            tags = listOf("notion", "android", "localization", "strings", "i18n", "automation")
+        }
+    }
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            pom {
+                name.set("Notion Stringboard Plugin")
+                description.set("A Gradle plugin that automatically generates Android string resources from Notion database")
+                url.set("https://github.com/lyh990517/notion-string-automation")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("lyh990517")
+                        name.set("lyh990517")
+                        email.set("lyh990517@naver.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/lyh990517/notion-string-automation.git")
+                    developerConnection.set("scm:git:ssh://github.com/lyh990517/notion-string-automation.git")
+                    url.set("https://github.com/lyh990517/notion-string-automation")
+                }
+            }
         }
     }
 }
